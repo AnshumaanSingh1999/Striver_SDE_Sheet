fb8502d51cf8c7044f22d74c92932d9d45c19ede
Anshumaan Singh <47323148+AnshumaanSingh1999@users.noreply.github.com>
2025-03-22 19:56:22 +0530

    Accepted solution

---
+++ String Part 2/z_function.java
@@ -0,0 +1,19 @@
+/*
+Accepted
+*/
+import java.util.Scanner;
+public class z_function {
+    public int strStr(String haystack, String needle) {
+        int occurrence=-1;
+        occurrence=haystack.indexOf(needle);
+        return occurrence;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String haystack=sc.nextLine();
+        String needle=sc.nextLine();
+        z_function zf=new z_function();
+        int occurrence=zf.strStr(haystack, needle);
+        System.out.println(occurrence);       
+    }
+}
